a9cf516e87d815c430b930c9332fff9520c6e260
waleedbaroudi <wbaroudi@hospitalonmobile.com>
2022-06-05 01:46:16 +0300

    init commit

---
+++ app/src/main/java/com/engr429/watcher/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.engr429.watcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.engr429.watcher.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        binding.tvS.text = "AYYYWA YA KOZI"
+    }
+}
