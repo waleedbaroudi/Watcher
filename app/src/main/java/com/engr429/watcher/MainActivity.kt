a65da62dea6a941efac0a24dcffa17f57d5ef419
waleedbaroudi <wbaroudi@hospitalonmobile.com>
2022-06-06 04:41:32 +0300

    Get image from bucket

--- app/src/main/java/com/engr429/watcher/MainActivity.kt
+++ app/src/main/java/com/engr429/watcher/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.engr429.watcher
 
 import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.annotation.ColorRes
+import com.bumptech.glide.Glide
 import com.engr429.watcher.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -27,7 +29,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getSceneFromCam() {
-
+        Glide.with(this).load("https://watcher-images.s3.eu-central-1.amazonaws.com/16ed484a911eef521f1de25df1844cbe").into(binding.imgScene)
+        binding.btnGetScene.setOnClickListener {
+            val bmp = (binding.imgScene.drawable as BitmapDrawable).bitmap
+            binding.imgScene.setImageBitmap(bmp)
+        }
     }
 
     private fun onButtonChecked(id: Int, isChecked: Boolean) {
