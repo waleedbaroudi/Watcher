a0adec83f3dc4635d924aafc99ee052e362115bd
waleedbaroudi <wbaroudi@hospitalonmobile.com>
2022-06-07 23:29:02 +0300

    Change Endpoint

--- app/src/main/java/com/engr429/watcher/Constants.kt
+++ app/src/main/java/com/engr429/watcher/Constants.kt
@@ -4,5 +4,5 @@ object Constants {
     const val IMAGE_KEY = "key"
     const val LABEL_KEY = "label"
     const val S3_URL = "https://watcher-images.s3.eu-central-1.amazonaws.com"
-    const val API_URL = "http://ec2-44-203-149-170.compute-1.amazonaws.com:8080/"
+    const val API_URL = "http://ec2-34-201-70-78.compute-1.amazonaws.com:8080/"
 }
